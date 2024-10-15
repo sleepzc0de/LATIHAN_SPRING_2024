47e44b4c224848139fef448936dba0feb4617a7f
sleepzc0de <auliyaputraazhari@gmail.com>
2024-10-15 11:44:49 +0700

    Setup Database BUKU

--- src/main/java/id/go/kemenkeu/perpustakaan_kemenkeu/controller/BukuController.java
+++ src/main/java/id/go/kemenkeu/perpustakaan_kemenkeu/controller/BukuController.java
@@ -17,12 +17,12 @@ public class BukuController {
     @GetMapping("/daftar")
     public String daftar(Model model) {
 
-        Buku buku1 = new Buku(1,"Buku 1","Penulis 1",2024);
-        Buku buku2 = new Buku(2,"Buku 2","Penulis 2",2024);
-        Buku buku3 = new Buku(3,"Buku 3","Penulis 3",2024);
-        Buku buku4 = new Buku(4,"Buku 4","Penulis 4",2024);
-        Buku buku5 = new Buku(5,"Buku 5","Penulis 5",2024);
-        Buku buku6 = new Buku(6,"Buku 6","Penulis 6",2024);
+        Buku buku1 = new Buku("Buku 1","Penulis 1",2024);
+        Buku buku2 = new Buku("Buku 2","Penulis 2",2024);
+        Buku buku3 = new Buku("Buku 3","Penulis 3",2024);
+        Buku buku4 = new Buku("Buku 4","Penulis 4",2024);
+        Buku buku5 = new Buku("Buku 5","Penulis 5",2024);
+        Buku buku6 = new Buku("Buku 6","Penulis 6",2024);
 
 
         List<Buku> bukuList = new ArrayList<>();
@@ -41,12 +41,12 @@ public class BukuController {
     @GetMapping("/daftar2")
     public ModelAndView daftar2(ModelAndView modelAndView) {
 
-        Buku buku1 = new Buku(1,"Buku 1","Penulis 1",2024);
-        Buku buku2 = new Buku(2,"Buku 2","Penulis 2",2024);
-        Buku buku3 = new Buku(3,"Buku 3","Penulis 3",2024);
-        Buku buku4 = new Buku(4,"Buku 4","Penulis 4",2024);
-        Buku buku5 = new Buku(5,"Buku 5","Penulis 5",2024);
-        Buku buku6 = new Buku(6,"Buku 6","Penulis 6",2024);
+        Buku buku1 = new Buku("Buku 1","Penulis 1",2024);
+        Buku buku2 = new Buku("Buku 2","Penulis 2",2024);
+        Buku buku3 = new Buku("Buku 3","Penulis 3",2024);
+        Buku buku4 = new Buku("Buku 4","Penulis 4",2024);
+        Buku buku5 = new Buku("Buku 5","Penulis 5",2024);
+        Buku buku6 = new Buku("Buku 6","Penulis 6",2024);
 
 
         List<Buku> bukuList = new ArrayList<>();
@@ -57,8 +57,7 @@ public class BukuController {
         bukuList.add(buku5);
         bukuList.add(buku6);
 
-        modelAndView.addObject("bukuList", bukuList);
-        modelAndView.setViewName("buku/daftar2");
+        modelAndView.addObject("bukuList", bukuList);        modelAndView.setViewName("buku/daftar2");
         return modelAndView;
     }
 }
--- src/main/java/id/go/kemenkeu/perpustakaan_kemenkeu/model/Buku.java
+++ src/main/java/id/go/kemenkeu/perpustakaan_kemenkeu/model/Buku.java
@@ -1,98 +1,36 @@
 package id.go.kemenkeu.perpustakaan_kemenkeu.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.util.UUID;
+@Setter
+@Getter
+@NoArgsConstructor
+@Accessors(chain = true)
+@Entity
 public class Buku {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     private String judul;
     private String penulis;
     private int tahunTerbit;
     private int harga;
     private char header;
 
-    public Buku(int id, String judul, String penulis, int tahunTerbit) {
-        this.id = id;
+    public Buku(String judul, String penulis, int tahunTerbit) {
         this.judul = judul;
         this.penulis = penulis;
         this.tahunTerbit = tahunTerbit;
     }
 
-    public Buku(int id, String judul, String penulis, int tahunTerbit, int harga, char header) {
-        this.id = id;
-        this.judul = judul;
-        this.penulis = penulis;
-        this.tahunTerbit = tahunTerbit;
-        this.harga = harga;
-        this.header = header;
-    }
-
-    public Buku(String judul) {
-        this.judul = judul;
-    }
 
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public char getHeader() {
-        return header;
-    }
-
-    public void setHeader(char header) {
-        this.header = header;
-    }
-
-    public String getJudul() {
-        return judul;
-    }
-
-    public void setJudul(String judul) {
-        this.judul = judul;
-    }
-
-    public String getPenulis() {
-        return penulis;
-    }
-
-    public void setPenulis(String penulis) {
-        this.penulis = penulis;
-    }
-
-    public int getTahunTerbit() {
-        return tahunTerbit;
-    }
-
-    public void setTahunTerbit(int tahunTerbit) {
-        this.tahunTerbit = tahunTerbit;
-    }
-
-    public int getHarga() {
-        return harga;
-    }
-
-    public void setHarga(int harga) {
-        this.harga = harga;
-    }
-
-    public void info(){
-        System.out.println("Judul: " + judul);
-        System.out.println("Penulis: " + penulis);
-        System.out.println("Tahun Terbit: " + tahunTerbit);
-        System.out.println("Harga: " + harga);
-    }
-    public void info(int tipe){
-        if(tipe == 1){
-            System.out.println("Judul: " + judul);
-            System.out.println("Penulis: " + penulis);
-        }else {
-            System.out.println("Judul: " + judul);
-            System.out.println("Penulis: " + penulis);
-            System.out.println("Tahun Terbit: " + tahunTerbit);
-        }
-    }
-    public void info(String tipe){
-
-    }
 }
